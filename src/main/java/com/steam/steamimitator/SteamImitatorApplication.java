fed1501c53df722aacb714c1edfd189ec8c0cdb7
Alexandru-Dan Daescu <aksion244@gmail.com>
2024-03-14 14:55:00 +0200

    Project initialization

---
+++ src/main/java/com/steam/steamimitator/SteamImitatorApplication.java
@@ -0,0 +1,13 @@
+package com.steam.steamimitator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamImitatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamImitatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steam/steamimitator/SteamImitatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steam.steamimitator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamImitatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
