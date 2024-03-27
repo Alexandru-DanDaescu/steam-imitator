7380d6211b347a53dd58ad401ef01e4dcfec73b5
Alexandru-Dan Daescu <aksion244@gmail.com>
2024-03-27 13:35:20 +0200

    Added interfaces and classes for services (unfinished).Added remaining annotaions to dtos.

--- src/main/java/com/steam/steamimitator/exceptions/VideoGameUpdateException.java
+++ src/main/java/com/steam/steamimitator/exceptions/VideoGameUpdateException.java
@@ -2,7 +2,7 @@ package com.steam.steamimitator.exceptions;
 
 public class VideoGameUpdateException extends RuntimeException{
 
-    public VideoGameUpdateException(String message){
-        super(message);
+    public VideoGameUpdateException(String message, Throwable cause){
+        super(message, cause);
     }
 }
---
+++ src/main/java/com/steam/steamimitator/services/AddressService.java
@@ -0,0 +1,4 @@
+package com.steam.steamimitator.services;
+
+public interface AddressService {
+}
